69c766ac1d8792e0e5d5c7b23231d0b952c2f19c
KAMER BERAT TOPAL <ktopal18@posta.pau.edu.tr>
2021-03-18 00:37:29 +0300

    Day 9 work 1 instructions has been done in this update.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if (car.DailyPrice > 0)
+                {
+                    _carDal.Add(car);
+                    Console.WriteLine("{0} added.", car.Description);
+                }
+                else
+                {
+                    Console.WriteLine("Daily price should be bigger than 0");
+                }
+        }
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+        public Car GetById(int Id)
+        {
+            return _carDal.Get(c => c.Id == Id);
+        }
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(b => b.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,34 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, CarRentalDatabaseContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (CarRentalDatabaseContext context = new CarRentalDatabaseContext())
+            {
+                var result = from p in context.Cars
+                             join b in context.Brands
+                             on p.BrandId equals b.Id
+                             join c in context.Colors
+                             on p.ColorId equals c.Id
+                             select new CarDetailDto {ColorName = c.ColorName, 
+                                 BrandName = b.BrandName, 
+                                 Description = p.Description, 
+                                 DailyPrice = p.DailyPrice };
+                return result.ToList();
+            }
+        }
+    }
+}
