0ae8f6010131671bd7b451928b8f88d427108fda
Kamer Berat Topal <43895483+Kamerthedev@users.noreply.github.com>
2021-03-19 23:33:47 +0300

    Update on ConsoleUI
    Manually added an entity. Code works fine.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
-﻿using Business.Concrete;
+using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -10,6 +11,15 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager1 = new CarManager(new EfCarDal());
+            carManager1.Add(new Car { 
+                Id = 1, 
+                BrandId = 1, 
+                ColorId = 2, 
+                DailyPrice = 150, 
+                Description = "Aile Arabası", 
+                ModelYear= 2009, 
+                Name="Audi" 
+            });
             foreach (var car in carManager1.GetAll())
             {
                 Console.WriteLine(car.Name);
